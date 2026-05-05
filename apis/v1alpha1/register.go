205767f23cb99f2fe4c98eeb89f0263fd7a092aa
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2026-05-05 14:59:46 +0300

    #1: rename template scaffold to provider-strapi

--- apis/v1alpha1/register.go
+++ apis/v1alpha1/register.go
@@ -9,7 +9,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "template.crossplane.io"
+	Group   = "strapi.crossplane.io"
 	Version = "v1alpha1"
 )
 
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/crossplane/provider-template
+module github.com/web-seven/provider-strapi
 
 go 1.24.0
 
